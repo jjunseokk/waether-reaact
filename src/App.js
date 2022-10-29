9df4e60f922644c092fc1bd7e9eeae294183b34f
junseok park <p.junseok53@gmail.com>
2022-10-29 10:27:42 +0900

    221029

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
       getWeatherByCurrentLoaction(lat, lon);
+      getLocation();
     });
   };
 
@@ -60,6 +61,36 @@ function App() {
     }
     console.log(apiError);
   };
+
+
+  // 모바일 GPS
+  const getLocation = () => {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            const now = new Date();
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    resolve({
+                        err: 0,
+                        time: now.toLocaleTimeString(),
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                    });
+                },
+                (err) => {
+                    resolve({
+                        err: -1,
+                        latitude: -1,
+                        longitude: -1,
+                    });
+                },
+                { enableHighAccuracy: true, maximumAge: 2000, timeout: 5000 }
+            );
+        } else {
+            reject({ error: -2, latitude: -1, longitude: -1 });
+        }
+    });
+}
 
 
   useEffect(() => {
